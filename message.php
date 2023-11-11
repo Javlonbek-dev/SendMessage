4891ef7d9ced45b35d5d623404212c6fa9494dc8
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-11-11 07:48:41 +0500

    Add: Necessary Functions

--- message.php
+++ message.php
@@ -2,8 +2,26 @@
 $name=$_POST['name'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
-$message=$_POST['message'];
 $website=$_POST['website'];
+$message=$_POST['message'];
 
-echo $email
+if(!empty($email)&& !empty($message)){
+ if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+    $reciever="javlonrahmatullayev07@gmail.com";
+    $subject="From:$name <$email>";
+    $body="Name: $name\nEmail:$email\nPhone:$phone\nWebsite:$website\n\nMessage:$message\n\nTRegards,\n$name";
+    $sender="From: $email";
+    if(mail($reciever,$subject,$body,$sender)){
+        echo "Your message has been sent";
+    }else{
+        echo "Sorry, failed to send your message";
+    }
+ }
+ else{
+    echo"Enter a valid email address";
+ }
+}else{
+    echo"Email and message required ";
+}
+
 ?>
